cb54ca43ffa6c738999f2492945178f32be8b08e
Li Chaoyi <lizhichaojiyingxiong@gmail.com>
2026-07-30 20:05:20 +0800

    Initial project scaffold: Kotlin JVM Gradle project with PLAN.md
    - matchsong: Android vocal profile & song recommendation app plan
    - Minimal Kotlin/JVM (Gradle 8.5, Kotlin 2.1.0, JDK 17 toolchain) scaffold
    - PLAN.md defines M-1..M11 milestones; this commit starts M-1

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/matchsong/Main.kt
@@ -0,0 +1,5 @@
+package matchsong
+
+fun main() {
+    println("matchsong")
+}
